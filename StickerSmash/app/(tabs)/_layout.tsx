b1ba7ebc9ee5a58b4c957d90973860d76c5d5408
bradyjohnson-tech <upsides.acclaim.0k@icloud.com>
2025-05-03 17:12:10 -0400

    Adding components to index

--- StickerSmash/app/(tabs)/_layout.tsx
+++ StickerSmash/app/(tabs)/_layout.tsx
@@ -1,42 +1,26 @@
-import { Tabs } from 'expo-router';
+import { StyleSheet, View } from 'react-native';
 
-import Ionicons from '@expo/vector-icons/Ionicons';
+import ImageViewer from '@/components/ImageViewer'; 
 
+const PlaceholderImage = require('@/assets/images/background-image.png');
 
-export default function TabLayout() {
+export default function Index() {
   return (
-    <Tabs
-  screenOptions={{
-    tabBarActiveTintColor: '#ffd33d',
-    headerStyle: {
-      backgroundColor: '#25292e',
-    },
-    headerShadowVisible: false,
-    headerTintColor: '#fff',
-    tabBarStyle: {
-    backgroundColor: '#25292e',
-    },
-  }}
->
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="about"
-        options={{
-          title: 'About',
-          tabBarIcon: ({ color, focused }) => (
-            <Ionicons name={focused ? 'information-circle' : 'information-circle-outline'} color={color} size={24}/>
-          ),
-        }}
-      />
-      
-    </Tabs>
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+  },
+});
