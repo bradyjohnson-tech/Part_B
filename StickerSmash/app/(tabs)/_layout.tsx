b3a4c32c91682362188c7a158c2d25cc185d4eac
bradyjohnson-tech <upsides.acclaim.0k@icloud.com>
2025-05-04 15:28:25 -0400

    Whoof that took a while to relize but I finally got the naviation tabs working again.

--- StickerSmash/app/(tabs)/_layout.tsx
+++ StickerSmash/app/(tabs)/_layout.tsx
@@ -1,26 +1,41 @@
-import { StyleSheet, View } from 'react-native';
+import { Tabs } from 'expo-router';
 
-import ImageViewer from '@/components/ImageViewer'; 
+import Ionicons from '@expo/vector-icons/Ionicons';
 
-const PlaceholderImage = require('@/assets/images/background-image.png');
 
-export default function Index() {
+export default function TabLayout() {
   return (
-    <View style={styles.container}>
-      <View style={styles.imageContainer}>
-        <ImageViewer imgSource={PlaceholderImage} />
-      </View>
-    </View>
+    <Tabs
+  screenOptions={{
+    tabBarActiveTintColor: '#ffd33d',
+    headerStyle: {
+      backgroundColor: '#25292e',
+    },
+    headerShadowVisible: false,
+    headerTintColor: '#fff',
+    tabBarStyle: {
+    backgroundColor: '#25292e',
+    },
+  }}
+>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="about"
+        options={{
+          title: 'About',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'information-circle' : 'information-circle-outline'} color={color} size={24}/>
+          ),
+        }}
+      />
+    </Tabs>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#25292e',
-    alignItems: 'center',
-  },
-  imageContainer: {
-    flex: 1,
-  },
-});
